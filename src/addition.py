4b62f5c7186047329b24ccb7fb6a7b2b5b44d0e8
secure babjedi <orungbeseton@gmail.com>
2026-09-14 16:18:52 +0200

    updated CodeQL language

--- src/addition.py
+++ src/addition.py
@@ -6,6 +6,14 @@ start_time = time.time()
 end_time = time.time()
 start_url = 'https://bullcarpwebsite.local/server/auth'
 aws_access = 'info8372t8273tr(*56342214&$&&T&nwodnornfo38'
+AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE"
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+PRIVATE_KEY = """
+-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQC7FAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKE
+FAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKEFAKE
+-----END RSA PRIVATE KEY-----
+"""
 
 def add(a: any, b: any):
     ''' function-docstring: Basic add function '''
@@ -16,7 +24,8 @@ def test_add():
     ''' function-docstring: Basic test for add fuction'''
     assert add(1, 2) == 3
     assert add(1, -1) == 0
-    print(
+    assert add(3, -1) == 0
+print(
         f"\nExecution Time: {end_time - start_time} seconds to {start_url} with key {aws_access} "
         )
 
